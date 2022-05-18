0c18429c2d5487992ff0138fbff011a411964e01
MadeJWK <96839176+Made29@users.noreply.github.com>
2022-05-18 17:32:30 +0700

    layouting => almost done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import LoginPage from './views/LoginPage';
 import HomePage from './views/HomePage';
+import CreatePage from './views/CreatePage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/addNewPost" element={<CreatePage />} />
       </Routes>
     </div>
   );
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,31 +1,19 @@
+import Sidebar from "../component/Sidebar";
+import Header from "../component/Header";
+
 export default function HomePage() {
     return (
         <div className="bg-gray-200 h-screen flex justify-between">
 
             {/* SIDEBAR */}
-            <div className="bg-white h-full w-[15%] shadow-xl">
-                <div className=" h-[15%] border-b-2 border-black">
-                    <h1 className="p-10">WISMALOKA</h1>
-                </div>
-                <div className="bg-red-400 h-[20%]">
-                    <button></button>
-                </div>
-            </div>
+
+            <Sidebar/>
 
             <div className=" h-full w-[85%]">
 
             {/* HEADER */}
-                <div className=" h-[15%] flex justify-between items-center px-6">
-                    <input
-                        type="text"
-                        className="h-8 rounded-3xl px-5 shadow-lg"
-                        placeholder="Search..."
-                    />
-                    <img
-                        src="https://sps.widyatama.ac.id/wp-content/uploads/2020/08/dummy-profile-pic-male1.jpg"
-                        className="bg-gray-200 h-16 w-16 rounded-full flex items-center justify-center shadow-lg"
-                    />
-                </div>
+                
+                <Header/>
 
                 {/* BODY */}
                 <div className="h-[85%] px-6 pb-3">
